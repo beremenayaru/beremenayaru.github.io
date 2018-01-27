98ea23af38e6f3eb8e645af7a6d72b77a0fe3611
Procrastinator <procrastinator@5kc.ru>
2018-01-27 21:33:14 +0300

    Init

---
+++ contakt.php
@@ -0,0 +1,52 @@
+<html>
+<head> 
+
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<link href="style.css" rel="stylesheet" type="text/css">
+</head> 
+<body>
+<p>
+<a href="http://www.mogaz.ru/" target="_blank">эксклюзивные футболки ,</a>. <a href="http://www.kd1.ru" target="_blank">шкафы-купе Новобратцевский</a>. Фондовый рынок: <a href="http://www.markets-today.ru" target="_blank">трейдинг ащкуч</a> акции, фьючерсы. www.cpbaudit.ru - <a href="http://www.cpbaudit.ru" target="_blank">регистрация фирм, курсы бухгалтерские, курсы бухгалтерского учета</a>. <a href="http://www.asvgroup.ru/" target="_blank">isover twin</a>. <a href="http://stopnikotin.ru/" target="_blank">как бросить курить</a>. <a href="http://www.sbody.ru" target="_blank">обертывание коррекция фигуры</a>. <a href="http://atan2002.ru/cataloguer/" target="_blank">спецодежда</a><table border="0" cellpadding="0" cellspacing="0" class="tbl1" width="104%">
+  <tr> 
+    <td colspan="4"></td>
+  </tr>
+  <tr valign="top"> 
+    <td width="60%" height="91"><img src="single_pixel.gif" width="1" height="140"></td>
+    <td width="40%" height="91" colspan="3" valign="top" align="center"> 
+
+    </td>
+  </tr>
+  <tr> 
+    <td colspan="3"  > 
+  <tr> 
+    <td valign="top" colspan="4"> 
+      <table width="90%" border="0" cellspacing="25">
+       <tr>
+        <td width="8%" valign="top"><img src="single_pixel.gif" width="140" height="1"><br>
+       <p>Meню<br>
+                        <a href="index.php">Главная</a><br>
+						 <a href="contakt.php">Контакты</a><br>
+						  <a href="friends.php">Друзья</a><br>
+						   
+        </td>
+        <td width="92%"> 
+
+                   <h2><font color="#FF6699">Сайт для ответственных родителей</font></h2>
+				   <br>
+				   <br>
+				   <br>
+				   <br>
+				   <br>
+				   <br>
+				   <br>
+<p>Дорогие друзья, по всех возникшувших у Вас вопросах, а также конструктивных предложениях и пожеланиях обращайтесь, пожалуйста, на электронный ящик <br>
+info@beremenaya.ru<p>
+Сайт создан с общеобразовательной целью и мы будем очень рады если он принесет Вам пользу. С уважением, администрация сайта.
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
---
+++ friends.php
@@ -0,0 +1,62 @@
+<html>
+<head> 
+
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<link href="style.css" rel="stylesheet" type="text/css">
+</head> 
+<body>
+<p>
+<a href="http://www.plasokna.ru" target="_blank">Пластиковые окна Петровско-Разумовская</a>. <a href="http://www.trade-city.ru/dir.php?id=128&from=31" target="_blank">спортивная одежда eterno интернет</a>. <a href="http://star-style.kiev.ua/education.php" target="_blank">обучение наращиванию ногтей</a>. <a href="http://www.artville-studio.ru/" target="_blank">модельное портфолио в мск</a><table border="0" cellpadding="0" cellspacing="0" class="tbl1" width="104%">
+  <tr> 
+    <td colspan="4"></td>
+  </tr>
+  <tr valign="top"> 
+    <td width="60%" height="91"><img src="single_pixel.gif" width="1" height="140"></td>
+    <td width="40%" height="91" colspan="3" valign="top" align="center"> 
+
+    </td>
+  </tr>
+  <tr> 
+    <td colspan="3"  > 
+  <tr> 
+    <td valign="top" colspan="4"> 
+      <table width="90%" border="0" cellspacing="25">
+       <tr>
+        <td width="8%" valign="top"><img src="single_pixel.gif" width="140" height="1"><br>
+       <p>Meню<br>
+                        <a href="index.php">Главная</a><br>
+						 <a href="contakt.php">Контакты</a><br>
+						  <a href="friends.php">Друзья</a><br>
+						   
+        </td>
+        <td width="92%"> 
+
+
+                   <h2><font color="#FF6699">Сайт для ответственных родителей</font></h2>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<p align=center>&nbsp;</p>
+<p align=center>&nbsp;</p>
+<p align=center>&nbsp;</p>
+<p align=center>&nbsp;</p>
+<p align=center>&nbsp;</p>
+<p align=center>&nbsp;</p>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<p>
+     
